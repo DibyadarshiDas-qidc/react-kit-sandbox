a4927bb6d0daddd53ea2a2ac3ce1b2cdb73b694a
DibyadarshiDas-qidc <dibyadarshi.das@quantuminventions.com>
2024-07-05 17:25:14 +0530

    handleChange

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { QIButton, QISwitch, QIInput } from '@qinvent/react-kit';
 import '@qinvent/react-kit/dist/styles/tailwind.css';
 import sprite from "@qinvent/react-kit/dist/assets/icons.svg"
@@ -6,21 +6,32 @@ import "./App.css";
 
 const MyComponent = () => {
 
+  const [value, setValue] = useState("")
+  const [Password, setPassword] = useState("")
+
   const handleButtonClick = () => {
     // Handle button click
     alert("Login Successfull")
   };
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value)
+  }
 
   return (
     <>
       <form action="">
         <div className='form-group' >
           <label htmlFor="">Name</label>
-          <QIInput />
+          <QIInput onChange={handleChange} value={value} />
         </div>
         <div className='form-group' >
-          <label htmlFor="">Password</label>
-          <QIInput />
+          <label  htmlFor="">Password</label>
+          <QIInput onChange={handleChangePassword} value={Password} />
         </div>
         <div className='form-group' >  
           <QISwitch />
